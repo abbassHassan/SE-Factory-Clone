70ef1c2ad7d17decf1e621832a8033673f5f2621
abbas hassan <abbass.hassan.a7@gmail.com>
2023-11-16 23:42:26 +0200

    add some sections

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
-console.log("1")
+document.addEventListener("DOMContentLoaded", function () {
+  var buttons = document.querySelectorAll(".show-more");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var contentContainer = button.closest(".faq-card");
+      var hiddenContent = contentContainer.querySelector(".hidden-content");
+      hiddenContent.style.display =
+        hiddenContent.style.display === "none" ? "block" : "none";
+    });
+  });
+});
